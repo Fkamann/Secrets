7b3bbb94a2e1a0f9183f44001f66ecde3c0d3d25
Fran <fran@email.com>
2022-11-09 11:57:35 +0100

    Replaced MD5 with Bcrypt

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,34 +36,33 @@ app.get("/register", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-    const newUser = new User( {
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User( {
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err) {
+            if(err)
+                console.log(err);
+            else
+                res.render("secrets");
+        });
     })
-
-    newUser.save(function(err) {
-        if(err)
-            console.log(err);
-        else
-            res.render("secrets");
-    });
 })
 
 app.post("/login", function(req,res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({email: username}, function(err, result) {
+    User.findOne({email: username}, function(err, foundUser) {
         if(err)
             console.log(err);
         else {
-            if(result) {
-                if(result.password === password) {
-                    res.render("secrets");
-                }
-                else {
-                    console.log("Email or password is incorrect or doesn't exist.")
-                }
+            if(foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result)
+                        res.render("secrets");
+                })
             }
         }
     })
